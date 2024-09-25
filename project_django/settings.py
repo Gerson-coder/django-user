63cdec62c890ce986c13f208c218904cc260e0a0
Gerson <gersonherrerach@gmail.com>
2024-09-24 20:25:00 -0500

    deploy 4.0

--- project_django/settings.py
+++ project_django/settings.py
@@ -126,10 +126,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+#necesario
 
-STATIC_URL = 'static/'
-STATICFILES_DIR =[os.path.join(BASE_DIR, 'home/static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 ALLOWED_HOSTS=['*']
+#necesario
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
